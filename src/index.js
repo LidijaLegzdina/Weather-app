eaa0dd4d38d47a1982c186fc6ceca390e545dbba
LidijaLegzdina <108259985+LidijaLegzdina@users.noreply.github.com>
2022-09-08 10:38:51 +0300

    Updated with final version
    Error with forecast

--- src/index.js
+++ src/index.js
@@ -18,6 +18,54 @@ let dayTime = `${day} ${hours}:${minutes}`;
 let currentDayTime = document.querySelector("#current-day-time");
 currentDayTime.innerHTML = dayTime;
 
+function formatDay(timeStamp) {
+  let date = new Date(timeStamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        ` 
+                
+          <div class="col-2 col-sm tue fs-6">
+                        <div class="weather-forecast-date">${formatDay(
+                          forecastDay.dt
+                        )}</div>
+            <br/>
+            <img class="fs-1" id="weather-forecast-icon" src="https://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"/>
+              <br />
+              <br />
+              <span class="weather-forecast-temp-max">
+              ${Math.round(forecastDay.temp.max)}°
+            </span>
+            <span class="weather-forecast-temp-min">
+              ${Math.round(forecastDay.temp.min)}°
+            </span>
+           `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "0fbf741dd6f046088a411342ceb1813f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${apiKey}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperatureRiga(response) {
   let temperatureDefault = document.querySelector("#current-temperature");
   let rigaTemperature = Math.round(response.data.main.temp);
@@ -39,6 +87,8 @@ function showTemperatureRiga(response) {
   let windRiga = Math.round(response.data.wind.speed);
   let windNowRiga = document.querySelector("#wind");
   windNowRiga.innerHTML = `${windRiga} m/s`;
+
+  getForecast(response.data.coord);
 }
 
 let apiKey = "0fbf741dd6f046088a411342ceb1813f";
